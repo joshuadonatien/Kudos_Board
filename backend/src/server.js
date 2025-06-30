bc439a594dbf823413dc477ab9b1cd654f6c76ca
janinejimen <jimenezj9@utexas.edu>
2025-06-30 11:55:56 -0700

    installed more dependencies and update the schema prisma

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,4 +7,8 @@ const boardRoutes = require("../routes/board-routes.js")
 app.use(express.json())
 app.use("/boards", boardRoutes)
 
+app.get('/', (req, res) => {
+    res.send("We are in the kudos board website")
+})
+
 app.listen(prototype, () => console.log(`Server running on port ${PORT}`))
