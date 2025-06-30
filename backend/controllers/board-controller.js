f7f78b0905b14ce90fdfa4beb1e595b8794f2446
janinejimen <jimenezj9@utexas.edu>
2025-06-30 13:51:25 -0700

    updated prisma and currently working on successfully running the backend

---
+++ backend/controllers/board-controller.js
@@ -0,0 +1,52 @@
+const prisma= require("../models/prisma-client")
+
+//grabbing all of the boards
+exports.getAllBoards = async(req, res) => {
+    let boards = await prisma.board.findAll()
+    res.json(boards)
+}
+
+//grabbing a specific board
+exports.getBoardById = async (req, res) => {
+    const id = Number(req.params.board_id)
+    const board = await prisma.findUnique({where: {id}})
+
+    if(!board) {
+        return res.status(404).json({error:"not found"})
+    }
+}
+
+//creating a new board
+exports.createBoard = async (req, res) => {
+    const {title, image_url, category, author} = req.body
+    const newBoard = await prisma.board.create({
+        data: {
+            title,
+            image_url,
+            category,
+            author
+        }
+    })
+
+    res.status(201).json(newBoard)
+}
+
+exports.updateBoard = async (req, res) => {
+    const id = Number(req.params.board_id)
+
+    const {title, image_url, category, author} = req.body
+    const updatedBoard = await prisma.board.update ({
+        where: {id},
+        data: {title, image_url, category, author}
+    })
+
+    res.json(updatedBoard)
+}
+
+exports.deleteBoard = async (req,res) => {
+    const id = Number(params.req.board_id)
+
+    await prisma.board.delete({where:{order_id: id}})
+    res.status(204).end()
+}
+
